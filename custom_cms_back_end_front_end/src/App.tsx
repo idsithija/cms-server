a79bffb152c53f3244a007f872b185647a689789
Sithija Sumanga <iidsithija@gmail.com>
2024-02-06 16:57:37 +0530

    fonts and other stuff added

--- custom_cms_back_end_front_end/src/App.tsx
+++ custom_cms_back_end_front_end/src/App.tsx
@@ -1,7 +1,42 @@
+import { Card } from "primereact/card";
 import "./styles/main.scss";
+import { InputText } from "primereact/inputtext";
+import { Password } from "primereact/password";
+import { Button } from "primereact/button";
 
-function App() {
-  return <>ss</>;
-}
+const LoginCardElement = () => {
+  return <Button label="Sign in" />;
+};
+
+const App = () => {
+  return (
+    <div className="h-screen">
+      <div className="h-full flex align-items-center">
+        <div className="w-20rem mx-auto">
+          <Card
+            title="Login"
+            className="w-full"
+            footer={() => LoginCardElement()}
+          >
+            <div className="flex flex-column gap-4">
+              <div className="p-inputgroup flex-1">
+                <span className="p-inputgroup-addon">
+                  <i className="pi pi-user"></i>
+                </span>
+                <InputText placeholder="Username" />
+              </div>
+              <div className="p-inputgroup flex-1">
+                <span className="p-inputgroup-addon">
+                  <i className="pi pi-lock"></i>
+                </span>
+                <Password className="w-full" placeholder="Password" />
+              </div>
+            </div>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
