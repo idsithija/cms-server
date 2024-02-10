ac7696a091eaf86984bf4894568edadcee4ed5c9
Sithija Sumanga <iidsithija@gmail.com>
2024-02-10 14:51:47 +0530

    build optimze

--- custom_cms_back_end_front_end/vite.config.ts
+++ custom_cms_back_end_front_end/vite.config.ts
@@ -1,11 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   build: {
-    outDir: '../custom_cms_back_end/build'
+    outDir: "../custom_cms_back_end/build",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.indexOf("node_modules") !== -1) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
-})
+});
