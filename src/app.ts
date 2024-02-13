d8c2a922ea6816f4a5c509b7a2f7fda747761752
Sithija Sumanga <iidsithija@gmail.com>
2024-02-13 15:05:11 +0530

    code update

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,21 @@
 import express from "express";
 import "express-async-errors";
 import morgan from "morgan";
-// import helmet from "helmet";
+import helmet from "helmet";
 import cors from "cors";
 import cookieSession from "cookie-session";
 
 import routes from "./routes";
 import sequelize from "./database/sequelize";
 import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errorHandlers/index";
+import { NotFoundError } from "./errorHandlers";
 import path from "path";
 
 const app = express();
 
-// app.set('trust proxy', true);
+app.set('trust proxy', true);
 app.use(morgan("dev"));
-// app.use(helmet());
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(
@@ -26,20 +26,20 @@ app.use(
   })
 );
 
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log("Connection has been established successfully.");
-//     return sequelize.sync();
-//   })
-//   .then(() => {
-//     console.log("All models were synchronized successfully.");
-//   })
-//   .catch((error) => {
-//     console.error("Unable to connect to the database: ", error);
-//   });
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+    return sequelize.sync();
+  })
+  .then(() => {
+    console.log("All models were synchronized successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
 
-// app.use(routes);
+app.use(routes);
 
 // Serve static files from the React build directory
 app.use(express.static(path.resolve(__dirname, '../views/dist')));
